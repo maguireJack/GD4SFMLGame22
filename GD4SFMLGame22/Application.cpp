4212c84d9c45341a2d37fc669f73a0da33f836aa
John Loane <john.loane@dkit.ie>
2021-11-26 09:42:28 +0000

    Settings State
    Note the extra toString() method in the utility class to display
    keys and the need to register the new state in the application
    class

--- GD4SFMLGame22/Application.cpp
+++ GD4SFMLGame22/Application.cpp
@@ -6,6 +6,7 @@
 #include "GameState.hpp"
 #include "MenuState.hpp"
 #include "PauseState.hpp"
+#include "SettingsState.hpp"
 
 const sf::Time Application::kTimePerFrame = sf::seconds(1.f / 60.f);
 
@@ -105,5 +106,6 @@ void Application::RegisterStates()
 	m_stack.RegisterState<MenuState>(StateID::kMenu);
 	m_stack.RegisterState<GameState>(StateID::kGame);
 	m_stack.RegisterState<PauseState>(StateID::kPause);
+	m_stack.RegisterState<SettingsState>(StateID::kSettings);
 
 }
--- GD4SFMLGame22/Utility.hpp
+++ GD4SFMLGame22/Utility.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+#include <SFML/Window/Keyboard.hpp>
 namespace sf
 {
 	class Sprite;
@@ -10,6 +12,7 @@ class Utility
 public:
 	static void CentreOrigin(sf::Sprite& sprite);
 	static void CentreOrigin(sf::Text& text);
+	static std::string toString(sf::Keyboard::Key key);
 
 };
 
